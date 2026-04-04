e88c331d734f429a055da528973b12c5fecaade0
Patrick Walczak <pwalczakx@gmail.com>
2026-04-04 11:21:31 +0200

    upgrade prisma, adapt files and config to the latest version

--- src/shared/lib/db/prisma.ts
+++ src/shared/lib/db/prisma.ts
@@ -1,9 +1,25 @@
-import { PrismaClient } from '@prisma/client';
+import 'dotenv/config';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from './generated/client';
 
-const globalForPrisma = global as unknown as { prisma?: PrismaClient };
+const connectionString = process.env.DATABASE_URL;
 
-const prisma: PrismaClient = globalForPrisma?.prisma ?? new PrismaClient();
+if (!connectionString) {
+	throw new Error('Missing DATABASE_URL');
+}
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+const globalForPrisma = globalThis as typeof globalThis & {
+	prisma?: PrismaClient;
+};
 
-export default prisma;
+const adapter = new PrismaPg({ connectionString });
+
+export const prisma =
+	globalForPrisma.prisma ??
+	new PrismaClient({
+		adapter,
+	});
+
+if (process.env.NODE_ENV !== 'production') {
+	globalForPrisma.prisma = prisma;
+}
