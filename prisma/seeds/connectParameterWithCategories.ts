9c8de627f8e658f4f9a96814535f0499a85acbae
Patrick Walczak <pwalczakx@gmail.com>
2026-05-27 15:59:39 +0200

    fix another error with Prisma

--- prisma/seeds/connectParameterWithCategories.ts
+++ prisma/seeds/connectParameterWithCategories.ts
@@ -1,6 +1,11 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from '../../src/shared/lib/db/generated/client';
+import { PrismaPg } from '@prisma/adapter-pg';
 
-const prisma = new PrismaClient();
+const adapter = new PrismaPg({
+	connectionString: process.env.DATABASE_URL!,
+});
+
+const prisma = new PrismaClient({ adapter });
 
 async function main() {
 	const parameter = await prisma.parameter.findUnique({
