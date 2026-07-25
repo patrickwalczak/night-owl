2bfd26ab8d6d24dc20e8c42c21d86b5aed61e99d
Patrick Walczak <pwalczakx@gmail.com>
2026-07-25 21:50:26 +0200

    Style the radio input

--- src/pages/home/ui/HomePage.tsx
+++ src/pages/home/ui/HomePage.tsx
@@ -1,16 +1,13 @@
-import { RadioInput } from '@/shared/ui/radioInput/RadioInput';
-
 import { CategoriesSection } from './categories/CategoriesSection';
 import { Hero } from './hero/Hero';
+import { TempRadio } from './TempRadio';
 
 export default function HomePage() {
     return (
         <main>
             <Hero />
             <CategoriesSection />
-            <div style={{ height: '300px', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-                <RadioInput label={'Label'} />
-            </div>
+            <TempRadio />
         </main>
     );
 }
