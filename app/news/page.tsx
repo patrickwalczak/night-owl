ee3d7554ed1b91c08d71e399581c53cc6c795242
Patrick Walczak <pwalczakx@gmail.com>
2026-08-22 13:54:50 +0200

    Remove the dummy news page and the swr package

--- app/news/page.tsx
+++
@@ -1,5 +0,0 @@
-import { NewsPage } from '@/pages/news';
-
-export default function NewsRoutePage() {
-    return <NewsPage />;
-}
--- src/pages/news/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as NewsPage } from './ui/NewsPage';
