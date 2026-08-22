5634db69719c9580d18d15646a72149bb7f41c50
Patrick Walczak <pwalczakx@gmail.com>
2026-08-22 14:01:12 +0200

    Remove the dummy catalog

--- src/pages/catalog/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as CatalogPage } from './ui/CatalogPage';
-export { default as CatalogStoreProvider } from './model/CatalogStoreProvider';
-export type { CatalogData } from './model/catalog.types';
--- src/pages/home/ui/HomePage.tsx
+++ src/pages/home/ui/HomePage.tsx
@@ -1,16 +1,11 @@
-import Link from 'next/link';
-
 import { CategoriesSection } from './categories/CategoriesSection';
 import { Hero } from './hero/Hero';
-import { TempRadio } from './TempRadio';
 
 export default function HomePage() {
     return (
         <main>
             <Hero />
             <CategoriesSection />
-            <TempRadio />
-            <Link href={'/product/demo-one'}>{'Product'}</Link>
         </main>
     );
 }
